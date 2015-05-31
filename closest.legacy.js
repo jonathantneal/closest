dad1e87381dc9847fdcdeb4628ad4113ce0c3e22
Bogdan Chadkin <trysound@yandex.ru>
2015-05-31 10:40:29 +0300

    Updated closest.legacy.js

--- closest.legacy.js
+++ closest.legacy.js
@@ -1,26 +1,31 @@
-(function (ELEMENT, PREFIX) {
-	ELEMENT.matches = ELEMENT.matches || ELEMENT[PREFIX + 'MatchesSelector'] || function (selector) {
-		var
-		element = this,
-		elements = (element.document || element.ownerDocument).querySelectorAll(selector),
-		index = 0;
+(function (ELEMENT) {
+	ELEMENT.matches = ELEMENT.matches
+		|| ELEMENT.msMatchesSelector
+		|| ELEMENT.mozMatchesSelector
+		|| ELEMENT.webkitMatchesSelector
+		|| function (selector) {
+			var
+			element = this,
+			elements = (element.document || element.ownerDocument).querySelectorAll(selector),
+			index = 0;
 
-		while (elements[index] && elements[index] !== element) ++index;
+			while (elements[index] && elements[index] !== element) {
+				++index;
+			}
 
-		return elements[index] ? true : false;
-	};
+			return elements[index] ? true : false;
+		};
 
 	ELEMENT.closest = ELEMENT.closest || function (selector) {
 		var node = this;
 
 		while (node) {
-			if (node.matches(selector)) return node;
-			else node = node.parentElement;
+			if (node.matches(selector)) {
+				break;
+			}
+			node = node.parentElement;
 		}
 
-		return null;
+		return node;
 	};
-})(
-	Element.prototype,
-	(this.getComputedStyle && [].join.call(getComputedStyle(document.documentElement, '')).match(/-(moz|ms|webkit)-/) || [])[1]
-);
+}(Element.prototype));
