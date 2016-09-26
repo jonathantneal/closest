5d4fe8d3726693ef978ad42773a59d04230e3c92
Shawn Allen <shawnbot@users.noreply.github.com>
2016-09-26 15:39:09 -0700

    wrap in IIFE with local Element reference
    This change wraps the polyfill in an IIFE that localizes the `Element` reference to `window.Element`, which fixes #16.

--- closest.js
+++ closest.js
@@ -1,31 +1,32 @@
 // element-closest | CC0-1.0 | github.com/jonathantneal/closest
+(function(Element) {
+	if (typeof Element.prototype.matches !== 'function') {
+		Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.mozMatchesSelector || Element.prototype.webkitMatchesSelector || function matches(selector) {
+			var element = this;
+			var elements = (element.document || element.ownerDocument).querySelectorAll(selector);
+			var index = 0;
 
-if (typeof Element.prototype.matches !== 'function') {
-	Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.mozMatchesSelector || Element.prototype.webkitMatchesSelector || function matches(selector) {
-		var element = this;
-		var elements = (element.document || element.ownerDocument).querySelectorAll(selector);
-		var index = 0;
-
-		while (elements[index] && elements[index] !== element) {
-			++index;
-		}
-
-		return Boolean(elements[index]);
-	};
-}
-
-if (typeof Element.prototype.closest !== 'function') {
-	Element.prototype.closest = function closest(selector) {
-		var element = this;
-
-		while (element && element.nodeType === 1) {
-			if (element.matches(selector)) {
-				return element;
+			while (elements[index] && elements[index] !== element) {
+				++index;
 			}
 
-			element = element.parentNode;
-		}
+			return Boolean(elements[index]);
+		};
+	}
 
-		return null;
-	};
-}
+	if (typeof Element.prototype.closest !== 'function') {
+		Element.prototype.closest = function closest(selector) {
+			var element = this;
+
+			while (element && element.nodeType === 1) {
+				if (element.matches(selector)) {
+					return element;
+				}
+
+				element = element.parentNode;
+			}
+
+			return null;
+		};
+	}
+})(window.Element);
