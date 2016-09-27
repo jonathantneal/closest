a681519c419946e71af83a658dcc7ba4eabc5750
Shawn Allen <shawn.allen@gsa.gov>
2016-09-27 10:43:18 -0700

    reference Element.prototype in closure

--- closest.js
+++ closest.js
@@ -1,21 +1,26 @@
 // element-closest | CC0-1.0 | github.com/jonathantneal/closest
-(function (Element) {
-	if (typeof Element.prototype.matches !== 'function') {
-		Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.mozMatchesSelector || Element.prototype.webkitMatchesSelector || function matches(selector) {
-			var element = this;
-			var elements = (element.document || element.ownerDocument).querySelectorAll(selector);
-			var index = 0;
+(function (ElementProto) {
 
-			while (elements[index] && elements[index] !== element) {
-				++index;
-			}
+	if (typeof ElementProto.matches !== 'function') {
 
-			return Boolean(elements[index]);
-		};
+		ElementProto.matches = ElementProto.msMatchesSelector ||
+			ElementProto.mozMatchesSelector ||
+			ElementProto.webkitMatchesSelector ||
+			function matches(selector) {
+				var element = this;
+				var elements = (element.document || element.ownerDocument).querySelectorAll(selector);
+				var index = 0;
+
+				while (elements[index] && elements[index] !== element) {
+					++index;
+				}
+
+				return Boolean(elements[index]);
+			};
 	}
 
-	if (typeof Element.prototype.closest !== 'function') {
-		Element.prototype.closest = function closest(selector) {
+	if (typeof ElementProto.closest !== 'function') {
+		ElementProto.closest = function closest(selector) {
 			var element = this;
 
 			while (element && element.nodeType === 1) {
@@ -29,4 +34,5 @@
 			return null;
 		};
 	}
-})(window.Element);
+
+})(window.Element.prototype);
