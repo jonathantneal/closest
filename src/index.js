6325d90420b2192f623dc8b19d068a33869d0304
Jonathan Neal <jonathantneal@hotmail.com>
2018-12-20 14:15:27 -0500

    3.0.0

---
+++ src/index.js
@@ -0,0 +1,33 @@
+export default function polyfill(window) {
+	const ElementPrototype = window.Element.prototype;
+
+	if (typeof ElementPrototype.matches !== 'function') {
+		ElementPrototype.matches = ElementPrototype.msMatchesSelector || ElementPrototype.mozMatchesSelector || ElementPrototype.webkitMatchesSelector || function matches(selector) {
+			const elements = (element.document || element.ownerDocument).querySelectorAll(selector);
+			let element = this;
+			let index = 0;
+
+			while (elements[index] && elements[index] !== element) {
+				++index;
+			}
+
+			return Boolean(elements[index]);
+		};
+	}
+
+	if (typeof ElementPrototype.closest !== 'function') {
+		ElementPrototype.closest = function closest(selector) {
+			let element = this;
+
+			while (element && element.nodeType === 1) {
+				if (element.matches(selector)) {
+					return element;
+				}
+
+				element = element.parentNode;
+			}
+
+			return null;
+		};
+	}
+}
